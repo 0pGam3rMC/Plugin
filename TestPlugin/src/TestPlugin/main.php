d1f15d372523acf03abffeff7af9a06df3ba8cfb
0pGam3rMC <kuasir321@gmail.com>
2019-09-16 18:40:22 -0400

    Update main.php

--- TestPlugin/src/TestPlugin/main.php
+++ TestPlugin/src/TestPlugin/main.php
@@ -1,1 +1,9 @@
+namespace TestPlugin;
 
+use pocketmine\plugin\Plugin;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+
+class extends Plugin
